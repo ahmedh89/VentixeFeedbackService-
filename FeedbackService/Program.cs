fa81d66483ff2c7dec5c763617530b4e41e1081c
ahmedh89 <ahhmed.hassan95@hotmail.com>
2025-05-15 12:02:55 +0200

    Done.

--- FeedbackService/Program.cs
+++ FeedbackService/Program.cs
@@ -1,19 +1,18 @@
 using FeedbackService.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("OpenCorsPolicy", policy =>
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader());
+    options.AddPolicy("OpenCors", p =>
+        p.AllowAnyOrigin()
+         .AllowAnyMethod()
+         .AllowAnyHeader());
 });
 
 
@@ -32,18 +31,17 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Feedback API v1");
+    c.RoutePrefix = "swagger"; 
+});
 
 app.UseHttpsRedirection();
 
 
-app.UseCors("OpenCorsPolicy");
-
+app.UseCors("OpenCors");
 
 app.MapHealthChecks("/health");
 app.MapControllers();
